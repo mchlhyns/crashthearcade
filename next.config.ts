2576e7173a1091c3eeb7915a7bf09e85850bcd3f
Mike Haynes <mikehayn.es@icloud.com>
2026-04-14 22:33:52 -0500

    Yet another OG image fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['127.0.0.1', '*.ngrok-free.app', '*.ngrok-free.dev'],
   outputFileTracingIncludes: {
-    '/[handle]/opengraph-image': [
+    '/*/opengraph-image': [
       './public/fonts/SpaceGrotesk/SpaceGrotesk-Bold.ttf',
       './public/fonts/SpaceMono/SpaceMono-Bold.ttf',
       './public/cta-wide-logo.png',
